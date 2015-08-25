4074371329fa943e178776c4aae2ff4a31fad35d
Jacob Sanford <jsanford@unb.ca>
2015-08-25 16:09:29 -0300

    Update API-M with docker mask

--- init/60_edit_fedora_xacml_policies.sh
+++ init/60_edit_fedora_xacml_policies.sh
@@ -8,8 +8,8 @@ rm -v ${FEDORA_HOME}/data/fedora-xacml-policies/repository-policies/default/deny
 # Add in Islandora specific policies
 cp -r ${TMP_WORKDIR}/islandora-xacml-policies ${FEDORA_HOME}/data/fedora-xacml-policies/repository-policies/islandora
 
-# Allow access from Drupal site
-sed -i -e "s|<AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">127.0.0.1</AttributeValue>|<AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">127.0.0.1</AttributeValue>\n           <AttributeValue DataType=\"http://www.w3.org/2001/XMLSchema#string\">${DRUPAL_PORT_80_TCP_ADDR}</AttributeValue>|g" ${FEDORA_HOME}/data/fedora-xacml-policies/repository-policies/default/deny-apim-if-not-localhost.xml
+# Allow API-M access from Docker Mask IP, failover to localhost
+sed -i '35i            <Apply FunctionId="urn:oasis:names:tc:xacml:1.0:function:regexp-string-match">\n        <AttributeValue DataType="http://www.w3.org/2001/XMLSchema#string">172\\.17\\.\\d{1,3}\\.\\d{1,3}</AttributeValue>\n          <Apply FunctionId="urn:oasis:names:tc:xacml:1.0:function:string-one-and-only">\n            <EnvironmentAttributeDesignator AttributeId="urn:fedora:names:fedora:2.1:environment:httpRequest:clientIpAddress" DataType="http://www.w3.org/2001/XMLSchema#string"/>\n          </Apply>\n          </Apply>' ${FEDORA_HOME}/data/fedora-xacml-policies/repository-policies/default/deny-apim-if-not-localhost.xml
 
 # Install auth filter in Fedora lib
 cp -v ${TMP_WORKDIR}/fcrepo-drupalauthfilter-${FEDORA_AUTH_FILTER_FEDORA_VERSION}.jar ${FEDORA_HOME}/tomcat/webapps/fedora/WEB-INF/lib
