15221f7120051e91c23624e3f2473c87b833e0e9
Jacob Sanford <jsanford@unb.ca>
2015-08-26 11:23:06 -0300

    Move to ENV driven fedora database properties

--- init/45_edit_mysql_host_in_install_properties.sh
+++ init/45_edit_mysql_host_in_install_properties.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-# Do in-place edits of MySQL address
-sed -i "s/MYSQL_HOST/$MYSQL_PORT_3306_TCP_ADDR/g" ${TMP_WORKDIR}/install.properties
-sed -i "s/MYSQL_PORT/$MYSQL_PORT_3306_TCP_PORT/g" ${TMP_WORKDIR}/install.properties
+MYSQL_PORT_3306_TCP_ADDR="${MYSQL_PORT_3306_TCP_ADDR:-$(echo $FEDORA_MYSQL_HOST)}"
+MYSQL_PORT_3306_TCP_PORT="${MYSQL_PORT_3306_TCP_PORT:-$(echo $FEDORA_MYSQL_PORT)}"
+
+# Do in-place edits of MySQL properties
+sed -i "s/FEDORA_MYSQL_HOST/$MYSQL_PORT_3306_TCP_ADDR/g" ${TMP_WORKDIR}/install.properties
+sed -i "s/FEDORA_MYSQL_PORT/$MYSQL_PORT_3306_TCP_PORT/g" ${TMP_WORKDIR}/install.properties
+sed -i "s/FEDORA_MYSQL_USER/$FEDORA_MYSQL_USER/g" ${TMP_WORKDIR}/install.properties
+sed -i "s/FEDORA_MYSQL_PASS/$FEDORA_MYSQL_PASS/g" ${TMP_WORKDIR}/install.properties
+sed -i "s/FEDORA_MYSQL_DB/$FEDORA_MYSQL_DB/g" ${TMP_WORKDIR}/install.properties
